1ed64b526c48a523537e9a1e93b42470ea9bc0b1
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-02-25 12:32:26 +0530

    This is a demo project for spring ai olllma

---
+++ src/test/java/com/spring_boot_ai_ollma/spring_ai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_ai_ollma.spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
